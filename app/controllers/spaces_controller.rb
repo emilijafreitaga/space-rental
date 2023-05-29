52f4079e3a9d4b08758e31ed0affadd9c1d340e9
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2023-05-29 18:03:52 +0100

    created not full spaces controller and indexes

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,39 @@
 class SpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  # skip_before_action :authenticate_user!, only: :index
+  def index
+    @spaces = Space.all
+  end
 
+  def show
+    @space = Space.find(params[:id])
+  end
+
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    @space.save
+    redirect_to spaces.path
+  end
+
+  def edit
+    @space = Space.find(param[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @space = Space.find(params[:id])
+    @space.destroy
+    redirect_to spaces.path
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:place_name, :address, :category, :description, :max_capacity, :price_per_booking)
+  end
 end

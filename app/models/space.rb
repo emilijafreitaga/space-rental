3eca22448b15a4d2827abc793b862ec2713fcf52
Emilija Freitaga <emilija.freitaga@gmail.com>
2023-05-30 17:25:07 +0100

    updated space, booking controllers; dashboard, space show, booking edit work but don't have any styling; updated user, space, booking models

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,8 @@ class Space < ApplicationRecord
   CATEGORY = %w[Party Wedding Birthday Coorporate]
 
   validates :category, inclusion: { in: CATEGORY }
+  validates :place_name, :address, :max_capacity, :category, :price_per_booking, presence: true
+  validates :price_per_booking, numericality: { greater_than: 0 }
 
   belongs_to :user
   has_many :bookings, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :spaces
   has_many :bookings
+  has_many :bookings_as_owner, through: :spaces, source: :bookings
 
   validates :first_name, :last_name, :email, :password, presence: true
 end
